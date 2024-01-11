1ccc95cdd4ba65f2ceff9a6b6bbb18b7c71fc99d
whtjddbs <jsy_1201@naver.com>
2024-01-11 23:30:04 +0900

    AppConfig를 통해 SOLID 원칙 중 3가지 SRP, DIP, OCP 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,14 +12,24 @@ import hello.core.order.OrderServiceImpl;
 
 /**
  * AppConfig를 통해 구체 클래스 주입을 담당하게 하고 각 서비스들은 역할에만 집중할 수 있다.
+ * 즉, "DI 컨테이너" 역할을 한다.
  */
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    /* 할인 정책 변경 시 discountPolicy에서 구현체만 변경하면 된다. */
+    private DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
